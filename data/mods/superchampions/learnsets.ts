dacb15f943de0a567fd299dcebee4e82bb981eda
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-05-30 08:57:41 -0400

    Dex

---
+++ data/mods/superchampions/learnsets.ts
@@ -0,0 +1,51 @@
+export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTable = {
+	alakazam: {
+		learnset: {
+			inherit: true,
+			teleport: ["9L1"],
+		},
+	},
+	charizard: {
+		learnset: {
+			inherit: true,
+			seismictoss: ["9L1"],
+		},
+	},
+	beedrill: {
+		learnset: {
+			inherit: true,
+			fly: ["9L1"],
+		},
+	},
+	kangaskhan: {
+		learnset: {
+			inherit: true,
+			dizzypunch: ["9L1"],
+		},
+	},
+	victreebel: {
+		learnset: {
+			inherit: true,
+			leaftornado: ["9L1"],
+		},
+	},
+	gliscor: {
+		learnset: {
+			inherit: true,
+			guillotine: ["9L1"],
+		},
+	},
+	lopunny: {
+		learnset: {
+			inherit: true,
+			dizzypunch: ["9L1"],
+		},
+	},
+	serperior: {
+		learnset: {
+			inherit: true,
+			leaftornado: ["9L1"],
+			aquatail: ["9L1"],
+		},
+	},
+};

2b71b4dbd66485568fd7c5c473b63b73769d273d
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-05-30 08:57:05 -0400

    Dex

--- data/mods/superchampions/scripts.ts
+++ data/mods/superchampions/scripts.ts
@@ -7,6 +7,22 @@ export const Scripts: ModdedBattleScriptsData = {
 		},
 	},
 	init() {
-
+		// Alakazam
+		this.modData("Learnsets", "alakazam").learnset.teleport = ["9L1"];
+		// Charizard
+		this.modData("Learnsets", "charizard").learnset.seismictoss = ["9L1"];
+		// Beedrill
+		this.modData("Learnsets", "beedrill").learnset.fly = ["9L1"];
+		// Kangaskhan
+		this.modData("Learnsets", "kangaskhan").learnset.dizzypunch = ["9L1"];
+		// Victreebel
+		this.modData("Learnsets", "victreebel").learnset.leaftornado = ["9L1"];
+		// Gliscor
+		this.modData("Learnsets", "gliscor").learnset.guillotine = ["9L1"];
+		// Lopunny
+		this.modData("Learnsets", "lopunny").learnset.dizzypunch = ["9L1"];
+		// Serperior
+		this.modData("Learnsets", "serperior").learnset.leaftornado = ["9L1"];
+		this.modData("Learnsets", "serperior").learnset.aquatail = ["9L1"];
 	}
 };
